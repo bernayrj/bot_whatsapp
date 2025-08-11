df454cb043412ce64806074f5b439ffe918b7671
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-08-11 16:10:21 -0400

    ws configuration

--- ws-server.js
+++ ws-server.js
@@ -1,19 +1,26 @@
 // ws-server.js
-const WebSocket = require('ws');
+const https = require("https");
+const fs = require("fs");
+const WebSocket = require("ws");
+const app = express();
 
-const wss = new WebSocket.Server({ port: 3001 }); // you can change port
+function setupWebSocket(server) {
+  // Create a WebSocket server instance linked to the HTTPS Server
+  const wss = new WebSocket.Server({ server, path: "/socket" }); // you can change port
 
-wss.on('connection', (ws) => {
-  console.log('Dashboard connected via WebSocket');
-});
+  wss.on("connection", (ws) => {
+    console.log("Dashboard connected via WebSocket");
+  });
+};
 
 // Utility to broadcast to all clients
 function broadcastNewOrder() {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ type: 'newOrder'}));
+      client.send(JSON.stringify({ type: "newOrder" }));
+      console.log("order sent from ws!");
     }
   });
 }
 
-module.exports = { broadcastNewOrder };
+module.exports = { broadcastNewOrder, setupWebSocket };
